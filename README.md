0e2e6a787e3044cb6059f909d5917282d1c446bd
shr1ftyy <syeamtechdemon@gmail.com>
2021-05-04 18:35:35 +0930

    updated some stuff

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This script visualized how the strength of the magnetic field on a body
 changes as the distance between the solenoid's air core and teh body changes.
 
 By Syeam Bin Abdullah
+
+![img](./img/img.png)
--- magnetism.py
+++ magnetism.py
@@ -5,14 +5,18 @@ from magpylib.source.magnet import Cylinder
 from magpylib.source.current import Circular
 from magpylib import Collection 
 
+TURNS = 15
+s = 90
+LENGTH = 3
+CURRENT = 1
+
 # create magnets
 s2 = Cylinder(mag=(0,0,500), dim=(3,5))
-coil1 = [Circular(curr=1,dim=3,pos=[0,0,z]) for z in np.linspace(-3,3,15)]
+coil1 = [Circular(curr=CURRENT,dim=LENGTH,pos=[0,0,z]) for z in np.linspace(-3,3,TURNS)]
 
-# create collection
+# create collection 
 c1 = Collection(coil1)
 
-s = 90
 xs = np.linspace(0, 0,6)
 ys = np.linspace(0, 0,6)
 zs = np.linspace(-9, 9, s)
@@ -28,7 +32,7 @@ print(mags)
 
 
 plt.plot(z_pos, mags)
-plt.title(f"Strength of Magnetic Field - Solenoid, 15 turns, 3cm long, 1 amp")
+plt.title(f"Strength of Magnetic Field - Solenoid, {TURNS} turns, {LENGTH}cm long, {CURRENT} amp, {s} data points")
 plt.xlabel('Displacement from center of air core (cm)')
 plt.ylabel(f'Magnetic Field Strength (mT)')
 
